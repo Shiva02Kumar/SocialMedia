8eceaf82b5125d68b8f5d27a3a804d880d2114e1
Shiva Kumar <shiva2002kumar@gmail.com>
2024-12-19 17:04:24 +0530

    server update

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,20 @@ app.use('/auth', authRouter);
 app.use('/chats', chatRouter)
 app.use('/messages', messageRouter)
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  })
+}
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully")
+  })
+}
+
+
 connectDb();
 
 app.listen(PORT, () => {
